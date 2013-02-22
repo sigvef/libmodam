21deb9ec2f14dcb5ab27fa09a9dad3a678c3ab55
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-02-22 22:30:31 +0100

    Started adding effects

--- include/MOD.h
+++ include/MOD.h
@@ -5,6 +5,25 @@
 #include "MOD_Sample.h"
 #include "MOD_Pattern.h"
 
+#define EFFECT_ARPEGGIO 0
+#define EFFECT_SLIDE_UP 1
+#define EFFECT_SLIDE_DOWN 2
+#define EFFECT_SLIDE_TO_NOTE 3
+#define EFFECT_VIBRATO 4
+#define EFFECT_CONTINUE_SLIDE_TO_NOTE_AND_VOLUME_SLIDE 5
+#define EFFECT_CONTINUE_VIBRATO_TO_NOTE_AND_VOLUME_SLIDE 6
+#define EFFECT_TREMOLO 7
+#define EFFECT_UNUSED 8
+#define EFFECT_SET_SAMPLE_OFFSET 9
+#define EFFECT_VOLUME_SLIDE 10
+#define EFFECT_POSITION_JUMP 11
+#define EFFECT_SET_VOLUME 12
+#define EFFECT_PATTERN_BREAK 13
+#define EFFECT_EXTRAS 14
+#define EFFECT_SET_SPEED 15
+
+
+
 /* http://www.mediatel.lu/workshop/audio/fileformat/h_mod.html */
 
 typedef struct {
--- src/Player_Channel.c
+++ src/Player_Channel.c
@@ -15,6 +15,58 @@ MOD_Player_Channel* MOD_Player_Channel_create(){
 
 uint8_t MOD_Player_Channel_step(MOD_Player_Channel* player_channel, MOD* mod, MOD_Channel* channel){
 
+        int e = (channel->effect&0xf00) >> 8;
+        int x = (channel->effect&0x0f0) >> 4;
+        int y =  channel->effect&0x00f;
+        switch(e){
+            case EFFECT_ARPEGGIO:
+                break;
+            case EFFECT_SLIDE_UP:
+                break;
+            case EFFECT_SLIDE_DOWN:
+                break;
+            case EFFECT_SLIDE_TO_NOTE:
+                break;
+            case EFFECT_VIBRATO:
+                break;
+            case EFFECT_CONTINUE_SLIDE_TO_NOTE_AND_VOLUME_SLIDE:
+                break;
+            case EFFECT_CONTINUE_VIBRATO_TO_NOTE_AND_VOLUME_SLIDE:
+                break;
+            case EFFECT_TREMOLO:
+                break;
+            case EFFECT_UNUSED:
+                break;
+            case EFFECT_SET_SAMPLE_OFFSET:
+                break;
+            case EFFECT_VOLUME_SLIDE:
+                break;
+            case EFFECT_POSITION_JUMP:
+                break;
+            case EFFECT_SET_VOLUME:
+                break;
+            case EFFECT_PATTERN_BREAK:
+                break;
+            case EFFECT_EXTRAS:
+                break;
+
+            case EFFECT_SET_SPEED:
+
+                ;int speed = x*16+y;
+                if(speed == 0){
+                    speed = 1;
+                }
+
+                if(speed > 32){
+                    speed = (24 * speed)/6;
+                }
+
+                //player->ticks_per_division = speed;  
+                break;
+            default:
+                break;
+        }
+
         uint8_t out;
         if(channel->sample == 0){
             channel->sample = player_channel->old_sample;
@@ -39,9 +91,9 @@ uint8_t MOD_Player_Channel_step(MOD_Player_Channel* player_channel, MOD* mod, MO
                 }
             }
 
-            out = player_channel->sample_tracker < mod->samples[channel->sample-1]->length ? 127+(uint8_t)mod->samples[channel->sample-1]->data[((int)player_channel->sample_tracker)%mod->samples[channel->sample-1]->length] : 0;
+            out = player_channel->sample_tracker < mod->samples[channel->sample-1]->length ? (127+(uint8_t)mod->samples[channel->sample-1]->data[((int)player_channel->sample_tracker)%mod->samples[channel->sample-1]->length]) : 127;
         }else{
-            out = 0;
+            out = 127;
         }
         player_channel->tick++;
         return out;
