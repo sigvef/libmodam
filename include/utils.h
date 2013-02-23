09a1dbfabc2317250d7e195d9dc9d9f73bb44868
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-02-23 10:21:34 +0100

    Fixed bug in LARP, added MIN

--- include/utils.h
+++ include/utils.h
@@ -1,9 +1,10 @@
 #ifndef MOD_UTILS_H
 #define MOD_UTILS_H
 
-#define LARP(A,B,T) ((A)*(T))+((B)*(1-(T)))
+#define LARP(A,B,T) ((B)*(T))+((A)*(1-(T)))
 
 #define MAX(A,B) ((A)<(B)?(B):(A))
+#define MIN(A,B) ((A)>(B)?(B):(A))
 
 int16_t MOD_utils_read16(int8_t **data);
 
