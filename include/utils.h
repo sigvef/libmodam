3d745505932abf018a2303ded27c651bff5cd927
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-03-08 07:37:43 +0100

    Yo

--- include/utils.h
+++ include/utils.h
@@ -6,11 +6,8 @@
 #define MAX(A,B) ((A)<(B)?(B):(A))
 #define MIN(A,B) ((A)>(B)?(B):(A))
 
-#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-    #define FLIP_ENDIANNESS_INT16(a) ((((a)&0xff)<<8)|(((a)&0xff00)>>8))
-#else
-    #define FLIP_ENDIANNESS_INT16(a) (a)
-#endif
+//#define FLIP_ENDIANNESS_INT16(a) ((((a)&0xff)<<8)|(((a)&0xff00)>>8))
+#define FLIP_ENDIANNESS_INT16(a) (a)
 
 int16_t MOD_utils_read16(const int8_t **data);
 
